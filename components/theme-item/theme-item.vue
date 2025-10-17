93103525e2335a1500477ebcd02dad378368aabb
hufanglei <690328661@qq.com>
2025-10-17 21:06:58 +0800

    init

---
+++ components/theme-item/theme-item.vue
@@ -0,0 +1,55 @@
+<template>
+	<view class="themeItem">
+		<navigator url="" class="box">
+			<image class="pic" src="../../common/images/classify1.jpg" mode=""></image>
+			<view class="mask">明星美女</view>
+			<view class="tab">3天前更新</view>
+		</navigator>
+	</view>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+.themeItem{
+	.box{
+		height: 340rpx;
+		border-radius: 10rpx;
+		overflow: hidden;
+		position: relative;
+		.pic{
+			width: 100%;
+		}
+		.mask{
+			width: 100%;
+			height: 70rpx;
+			position: absolute;
+			bottom: 0;
+			left: 0;
+			background: rgba(0,0,0,0.2);
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			color: #fff;
+			font-weight: 600;
+			// 磨砂效果
+			backdrop-filter: blur(20rpx);
+			font-size: 30rpx;
+		}
+		.tab{
+			position: absolute;
+			left: 0;
+			top: 0;
+			background: rgba(255,129,90,0.7);
+			// 磨砂效果
+			backdrop-filter: blur(20rpx);
+			color: #fff;
+			font-size: 22rpx;
+			padding: 6rpx 14rpx;
+			border-radius: 0 0 20rpx 0;
+		}
+	}
+}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,191 @@
+<template>
+	<view class="homeLayOut">
+		<view class="banner">
+			<swiper circular indicator-dots indicator-color="rgba(255,255,255,0.5)" indicator-active-color="#fff" autoplay>
+				<swiper-item >
+					<image src="../../common/images/banner1.jpg" mode="aspectFill"></image>
+				</swiper-item>
+				<swiper-item>
+					<image src="../../common/images/banner2.jpg" mode="aspectFill"></image>
+				</swiper-item>
+				<swiper-item>
+					<image src="../../common/images/banner3.jpg" mode="aspectFill"></image>
+				</swiper-item>
+			</swiper>
+		</view>
+		
+		<view class="notice">
+			<view class="left"> 
+				<uni-icons type="sound-filled" size="20" color="#28b389"></uni-icons>
+                <text class="text">公告</text>
+			</view>
+			<view class="center">
+				<swiper vertical autoplay interval="1500" duration="300" circular>
+					<swiper-item v-for="item in 4">
+						文字内容fdsafdasfdsda就是大家咖啡和电视剧看哈sd卡
+					</swiper-item>
+				</swiper>
+			</view>
+			<view class="right">
+				<uni-icons type="forward" size="16" color="#333"></uni-icons>
+			</view>
+		</view>
+		
+		<view class="select">
+			<common-title>
+				<template #name>每日推荐</template>
+				<template #custom>
+					<view class="date">
+						<uni-icons type="calendar" size="18" color="#289389"></uni-icons>
+						<view class="text">
+							<uni-dateformat :date="Date.now()" format="dd日"></uni-dateformat>
+						</view>
+					</view>
+				</template>
+			</common-title>
+			<view class="content">
+				<scroll-view  scroll-x>
+					<view class="box" v-for="item in 8">
+						<image src="../../common/images/preview_small.webp" mode="aspectFill"></image>
+					</view>
+				</scroll-view>
+			</view>
+		</view>
+		
+		<view class="theme">
+			<common-title>
+				<template #name>专题精选</template>
+				<template #custom>
+					<navigator url="" class="more">More+</navigator>
+				</template>
+			</common-title>
+			
+			<view class="content">
+				<theme-item v-for="item in 8"></theme-item>
+			</view>
+		</view>
+		
+	</view>
+</template>
+
+<script setup>
+	
+</script>
+
+<style lang="scss" scoped>
+.homeLayOut{
+	.banner{
+		width: 750rpx;
+		padding: 30rpx 0;
+		swiper{
+			width: 750rpx;
+			height: 340rpx;
+			&-item {
+				width: 100%;
+				height: 100%;
+				padding: 0 30rpx;
+				image{
+					width: 100%;
+					height: 100%;
+					border-radius: 10rpx;
+				}
+			}
+		}
+	}
+	
+	.notice {
+		width: 690rpx;
+		height: 80rpx;
+		line-height: 80rpx;
+		background: #f9f9f9;
+		// background: rgba(0,0,0,0.2);
+		margin: 0 auto;
+		border-radius: 80rpx;
+		display: flex;
+		// align-items: center;
+		// justify-content: space-between;
+		.left{
+			width: 140rpx;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			.text{
+				color: #28b389;
+				font-weight: 600;
+				font-size: 28rpx;
+			}
+		}
+		.center{
+			flex: 1;
+			swiper{
+				height: 100%;
+				&-item{
+					height: 100%;
+					font-size: 30rpx;
+					color: #666;
+					overflow: hidden;
+					white-space: nowrap;
+					text-overflow: ellipsis;
+				}
+			}
+		}
+		.right{
+			width: 70rpx;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+		}
+	}
+	
+	.select {
+		padding-top: 50rpx;
+		.date{
+			color: #28b389; 
+			display: flex;
+			align-items: center;
+			.text{
+				margin-left: 5rpx;
+			}
+		}
+		.content{
+			width: 720rpx;
+			margin-left: 30rpx;
+			margin-top: 30rpx;
+			scroll-view {
+				white-space: nowrap;
+				.box{
+					width: 200rpx;
+					height: 430rpx;
+					margin-right: 15rpx;
+					display: inline-block;
+					image{
+						width: 100%;
+						height: 100%;
+						border-radius: 10rpx;
+					}
+				}
+				.box:last-child{
+					margin-right: 30rpx;
+				}
+			}
+		}
+	}
+	
+	.theme {
+		padding-top: 50rpx;
+		.more{
+			font-size: 32rpx;
+			color: #888;
+		}
+		
+		.content{
+			margin-top: 30rpx;
+		    padding: 0 30rpx;
+			display: grid;
+			gap: 15rpx;
+			grid-template-columns: repeat(3, 1fr);			
+		}
+	}
+}	
+	
+</style>
